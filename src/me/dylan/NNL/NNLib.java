4366ccef919353761e61c1ddc306894e52a743d8
Dylan Katz <dylanishappy1@gmail.com>
2014-02-15 14:26:15 -0700

    Uploaded code

---
+++ src/me/dylan/NNL/NNLib.java
@@ -0,0 +1,12 @@
+package me.dylan.NNL;
+
+import java.util.Random;
+
+public class NNLib {
+	public static Random rand = new Random();
+	public static NNLib instance = new NNLib();
+	
+	public enum NodeType {
+		INPUT, OUTPUT, HIDDEN;
+	}
+}
---
+++ src/me/dylan/NNL/NNetwork.java
@@ -0,0 +1,27 @@
+package me.dylan.NNL;
+
+import java.util.ArrayList;
+
+/**
+ * A wrapper around an entire neural network.
+ * 
+ * @author Dylan
+ * 
+ */
+public class NNetwork {
+	private ArrayList<Neuron> connections = new ArrayList<Neuron>();
+
+	public NNetwork(ArrayList<Input> sensors, ArrayList<Neuron> connections,
+			ArrayList<Output> outputs) {
+		this.connections = connections;
+	}
+	public NNetwork(){};
+
+	public ArrayList<Neuron> getConnections() {
+		return connections;
+	}
+
+	public void addNeuron(Neuron neuron) {
+		connections.add(neuron);
+	}
+}
