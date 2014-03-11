ee82790ba20ef2484d84badb2283e88b726bde6b
Craig Gleckman <cgleckman@gmail.com>
2014-03-10 23:48:48 -0700

    No javadocs needed for NNL.Visualizer -- Added TODOS for later, and to check with Dylan on if we want to javadocs these files

--- src/me/dylan/NNL/Visualizer/Display.java
+++ src/me/dylan/NNL/Visualizer/Display.java
@@ -23,121 +23,128 @@ import javax.swing.JPanel;
  * @author Dylan
  * 
  */
+// TODO: No javadocs for this file since pretty self explanatory. Will need to
+// have javadocs added in future for release and/or when its determined
+// necessary
 public class Display {
-    private static JFrame main;
-    private static JPanel displayPanel;
-    private static Graphics graphics;
-    private static Robot bot;
-    private static int width = 0;
-    static ArrayList<Character> keysDown = new ArrayList<Character>();
-    public static void showDisplay(String title, Dimension size,
-	    Color backgroundFill) {
-	try {
-	    bot = new Robot();
-	} catch (AWTException e) {
-	    e.printStackTrace();
+	private static JFrame main;
+	private static JPanel displayPanel;
+	private static Graphics graphics;
+	private static Robot bot;
+	private static int width = 0;
+	static ArrayList<Character> keysDown = new ArrayList<Character>();
+
+	public static void showDisplay(String title, Dimension size,
+			Color backgroundFill) {
+		try {
+			bot = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		main = new JFrame(title);
+		displayPanel = new JPanel();
+		displayPanel.setSize(main.getSize());
+		displayPanel.setBackground(backgroundFill);
+		displayPanel.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent evt) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent evt) {
+				keysDown.remove(evt.getKeyChar());
+			}
+
+			@Override
+			public void keyPressed(KeyEvent evt) {
+				keysDown.add(evt.getKeyChar());
+			}
+		});
+		main.setSize(size);
+		main.add(displayPanel);
+		main.setVisible(true);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		graphics = displayPanel.getGraphics();
 	}
-	main = new JFrame(title);
-	displayPanel = new JPanel();
-	displayPanel.setSize(main.getSize());
-	displayPanel.setBackground(backgroundFill);
-	displayPanel.addKeyListener(new KeyListener() {
-	    @Override
-	    public void keyTyped(KeyEvent evt) {
-	    }
-	    
-	    @Override
-	    public void keyReleased(KeyEvent evt) {
-		keysDown.remove(evt.getKeyChar());
-	    }
-	    
-	    @Override
-	    public void keyPressed(KeyEvent evt) {
-		keysDown.add(evt.getKeyChar());
-	    }
-	});
-	main.setSize(size);
-	main.add(displayPanel);
-	main.setVisible(true);
-	main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	graphics = displayPanel.getGraphics();
-    }
-    
-    
-    public static int getWidth() {
-	return main.getWidth();
-    }
 
-    public static int getHeight() {
-	return main.getHeight();
-    }
-    public static boolean isKeyDown(char key) {
-	return keysDown.contains(key);
-    }
-    public static void clearRect(int x, int y, int width, int height) {
-	getGraphics().clearRect(x, y, width, height);
-    }
+	public static int getWidth() {
+		return main.getWidth();
+	}
 
-    public static void addMouseMotionListener(MouseMotionListener listener) {
-	displayPanel.addMouseMotionListener(listener);
-    }
+	public static int getHeight() {
+		return main.getHeight();
+	}
 
-    public static void addMouseListener(MouseListener listener) {
-	displayPanel.addMouseListener(listener);
-    }
+	public static boolean isKeyDown(char key) {
+		return keysDown.contains(key);
+	}
 
-    public static void setStrokeWidth(int width) {
-	Display.width = width;
-	((Graphics2D) getGraphics()).setStroke(new BasicStroke(width));
-    }
-    public static int getStrokeWidth() {
-	return width;
-    }
-    public static Graphics getGraphics() {
-	return graphics;
-    }
+	public static void clearRect(int x, int y, int width, int height) {
+		getGraphics().clearRect(x, y, width, height);
+	}
 
-    public static void setDisplayBackgroundColor(Color c) {
-	getGraphics().setColor(c);
-    }
+	public static void addMouseMotionListener(MouseMotionListener listener) {
+		displayPanel.addMouseMotionListener(listener);
+	}
 
-    public static void fillOval(int x, int y, int width, int height) {
-	getGraphics().fillOval(x, y, width, height);
-    }
+	public static void addMouseListener(MouseListener listener) {
+		displayPanel.addMouseListener(listener);
+	}
 
-    public static void drawString(int x, int y, String value) {
-	getGraphics().drawString(value, x, y);
-    }
+	public static void setStrokeWidth(int width) {
+		Display.width = width;
+		((Graphics2D) getGraphics()).setStroke(new BasicStroke(width));
+	}
 
-    public static void drawLine(int x, int y, int x2, int y2) {
-	getGraphics().drawLine(x, y, x2, y2);
-    }
+	public static int getStrokeWidth() {
+		return width;
+	}
 
-    public static void repaint() {
-	displayPanel.update(getGraphics());
-	displayPanel.revalidate();
-    }
+	public static Graphics getGraphics() {
+		return graphics;
+	}
 
-    public static void fillRect(int x, int y, int width, int height) {
-	getGraphics().fillRect(x, y, width, height);
-    }
+	public static void setDisplayBackgroundColor(Color c) {
+		getGraphics().setColor(c);
+	}
 
-    public static Color getDisplayBackgroundColor() {
-	return getGraphics().getColor();
-    }
+	public static void fillOval(int x, int y, int width, int height) {
+		getGraphics().fillOval(x, y, width, height);
+	}
 
-    public static void setOffset(Point offset) {
-	getGraphics().translate(offset.x, offset.y);
-    }
-    
-    public static Point getWindowLocOnScreen() {
-	return main.getLocationOnScreen();
-    }
-    
-    public static void setMouseLocation(Point location) {
-	bot.mouseMove((int)location.getX(), (int)location.getY());
-    }
-    // public static void updateDoubleBuffer() {
-    // displayPanel.update(getGraphics());
-    // }
+	public static void drawString(int x, int y, String value) {
+		getGraphics().drawString(value, x, y);
+	}
+
+	public static void drawLine(int x, int y, int x2, int y2) {
+		getGraphics().drawLine(x, y, x2, y2);
+	}
+
+	public static void repaint() {
+		displayPanel.update(getGraphics());
+		displayPanel.revalidate();
+	}
+
+	public static void fillRect(int x, int y, int width, int height) {
+		getGraphics().fillRect(x, y, width, height);
+	}
+
+	public static Color getDisplayBackgroundColor() {
+		return getGraphics().getColor();
+	}
+
+	public static void setOffset(Point offset) {
+		getGraphics().translate(offset.x, offset.y);
+	}
+
+	public static Point getWindowLocOnScreen() {
+		return main.getLocationOnScreen();
+	}
+
+	public static void setMouseLocation(Point location) {
+		bot.mouseMove((int) location.getX(), (int) location.getY());
+	}
+	// public static void updateDoubleBuffer() {
+	// displayPanel.update(getGraphics());
+	// }
 }
--- src/me/dylan/NNL/Visualizer/NodePaint.java
+++ src/me/dylan/NNL/Visualizer/NodePaint.java
@@ -6,38 +6,42 @@ import java.awt.Point;
 import me.dylan.NNL.Node;
 
 public class NodePaint {
-    Point nodeLocation = new Point(0, 0);
-    int size;
-    Color color;
+	Point nodeLocation = new Point(0, 0);
+	int size;
+	Color color;
 
-    /**
-     * This class is for painting all nodes(HiddenNodes, inputs, outputs)
-     * 
-     * @param x
-     * @param nodeColor
-     *            The color of the node
-     */
-    public NodePaint(int size, Color nodeColor) {
-	color = nodeColor;
-	this.size = size;
-    }
+	/**
+	 * This class is for painting all nodes(HiddenNodes, inputs, outputs)
+	 * 
+	 * @param x
+	 * @param nodeColor
+	 *            The color of the node
+	 */
+	// TODO: No javadocs for this file since pretty self explanatory. Will need
+	// to
+	// have javadocs added in future for release and/or when its determined
+	// necessary
+	public NodePaint(int size, Color nodeColor) {
+		color = nodeColor;
+		this.size = size;
+	}
 
-    public Point getPaintCoords() {
-	return nodeLocation;
-    }
+	public Point getPaintCoords() {
+		return nodeLocation;
+	}
 
-    public void paintNode(int x, int y, Node nodeToPaint) {
-	nodeLocation = new Point(x, y);
-	Color original = Display.getDisplayBackgroundColor();
-	Display.setDisplayBackgroundColor(color);
-	if (nodeToPaint.isActive())
-	    Display.setDisplayBackgroundColor(Color.RED);
-	Display.fillOval(nodeLocation.x, nodeLocation.y, size, size);
-	Display.drawString(x, y, nodeToPaint.getNodeInfo().getData());
-	Display.setDisplayBackgroundColor(original);
-    }
+	public void paintNode(int x, int y, Node nodeToPaint) {
+		nodeLocation = new Point(x, y);
+		Color original = Display.getDisplayBackgroundColor();
+		Display.setDisplayBackgroundColor(color);
+		if (nodeToPaint.isActive())
+			Display.setDisplayBackgroundColor(Color.RED);
+		Display.fillOval(nodeLocation.x, nodeLocation.y, size, size);
+		Display.drawString(x, y, nodeToPaint.getNodeInfo().getData());
+		Display.setDisplayBackgroundColor(original);
+	}
 
-    public void setColor(Color c) {
-	this.color = c;
-    }
+	public void setColor(Color c) {
+		this.color = c;
+	}
 }
